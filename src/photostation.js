f9e2b27b7df0ebf8d7f3b0fe431927ef6824aab9
Santosh Shrestha <santosh@rumsan.com>
2019-11-29 00:14:42 +0545

    added more photostation methods including photo download

--- src/photostation.js
+++ src/photostation.js
@@ -8,12 +8,15 @@ http.defaults.withCredentials = true;
 http.defaults.jar = new CookieJar();
 axiosCookieJarSupport(http);
 
-const request = async ({ url, data }) => {
-  let options = {
-    method: "POST",
-    data: qs.stringify(data),
-    url
-  };
+const request = async opts => {
+  let options = Object.assign(
+    {
+      method: "POST"
+    },
+    opts
+  );
+
+  if (opts.data) options.data = qs.stringify(opts.data);
   try {
     let { data } = await http(options);
     return data;
@@ -26,9 +29,11 @@ class PhotoStation {
   constructor(syno) {
     this.syno = syno;
     this.url = `${syno.ps_url}/photo/webapi`;
+    this.token = null;
   }
 
   async login() {
+    if (this.token) return this.token;
     let data = {
       api: "SYNO.PhotoStation.Auth",
       method: "photo_login",
@@ -37,41 +42,112 @@ class PhotoStation {
       password: this.syno.password,
       enable_syno_token: true
     };
-    try {
-      let resData = await request({ url: `${this.url}/auth.php`, data });
-      if (resData.success) {
-        this.token = resData.data.sid;
-        return resData;
-      }
-      return resData;
-    } catch (e) {
-      console.log(e);
+    let resData = await request({ url: `${this.url}/auth.php`, data });
+    if (resData.success) {
+      console.log("===== Logged In ==========");
+      this.token = resData.data.sid;
+      return this.token;
+    } else {
+      throw Error("Login unsuccessful");
     }
   }
 
-  async listAlbums() {
+  async listAlbums(options) {
     await this.login();
 
-    let data = {
-      api: "SYNO.PhotoStation.Album",
-      method: "list",
-      version: 1,
-      offset: 0,
-      limit: -1,
-      recursive: false,
-      type: "album",
-      additional: "album_permission",
-      sort_direction: "desc",
-      sort_by: "preference",
-      ignore: "thumbnail"
-    };
+    let data = Object.assign(
+      {
+        api: "SYNO.PhotoStation.Album",
+        method: "list",
+        version: 1,
+        offset: 0,
+        limit: 250,
+        recursive: false,
+        type: "album,photo,video",
+        additional:
+          "album_permission,photo_exif,video_codec,video_quality,thumb_size,file_location",
+        sort_direction: "desc",
+        sort_by: "preference"
+      },
+      options
+    );
 
-    let resData = await request({
+    return request({
       url: `${this.url}/album.php?SynoToken=${this.token}`,
       data
     });
+  }
 
-    return resData.data;
+  async getAlbumInfo(options) {
+    await this.login();
+
+    let data = Object.assign(
+      {
+        api: "SYNO.PhotoStation.Album",
+        method: "getinfo",
+        version: 1,
+        type: "album,photo,video",
+        additional: "album_sorting,item_count"
+      },
+      options
+    );
+
+    return request({
+      url: `${this.url}/album.php?SynoToken=${this.token}`,
+      data
+    });
+  }
+
+  async getPhotoInfo(options) {
+    await this.login();
+
+    let data = Object.assign(
+      {
+        api: "SYNO.PhotoStation.Photo",
+        method: "getexif",
+        version: 1
+      },
+      options
+    );
+
+    return request({
+      url: `${this.url}/photo.php`,
+      data
+    });
+  }
+
+  async updatePhotoRating(id, rating) {
+    return this.updatePhotoInfo({
+      id,
+      rating
+    });
+  }
+
+  async updatePhotoInfo(options) {
+    await this.login();
+
+    let data = Object.assign(
+      {
+        api: "SYNO.PhotoStation.Photo",
+        version: 1
+      },
+      options
+    );
+    data.method = "edit";
+    console.log(`${this.url}/photo.php?SynoToken=${this.token}`);
+
+    return request({
+      url: `${this.url}/photo.php?SynoToken=${this.token}`,
+      data
+    });
+  }
+
+  getPhoto(id, size = "large") {
+    return request({
+      method: "GET",
+      responseType: "stream",
+      url: `${this.url}/thumb.php?api=SYNO.PhotoStation.Thumb&method=get&version=1&size=${size}&id=${id}&rotate_version=0&SynoToken=${this.token}`
+    });
   }
 }
 
